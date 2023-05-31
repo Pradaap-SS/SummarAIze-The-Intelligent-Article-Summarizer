e4241b9cf36e12ce716eda3de257a88ff93901da
“Pradaap-SS” <pradaapss@gmail.com>
2023-05-31 16:08:28 -0500

    #5 Building the HomePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Hero from './components/Hero'
-import Demo from './components/Demo'
-
-import "./App.css"
-
-import App from "./App"
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )
